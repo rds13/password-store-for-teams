34eaf15519e2a10c275312298d844eded94b0c1c
Romuald du Song <romuald.dusong@deveryware.com>
2016-10-06 21:04:41 +0200

    disable completion until it's fixed

--- .load.zsh
+++ .load.zsh
@@ -6,12 +6,12 @@ __tmp_pass_name=$(basename -- $__tmp_pass_dir | sed 's/^\.*//')
 alias $__tmp_pass_name="PASSWORD_STORE_DIR=$__tmp_pass_dir pass"
 
 # Small hack to allow use alternate path for PASSWORD_STORE_DIR in zsh completion
-if type _pass > /dev/null 2>&1; then
-  compdef _pass teampass
-  zstyle ':completion::complete:teampass::' prefix "${__tmp_pass_dir}/"
-  teampass() {
-    PASSWORD_STORE_DIR="${__tmp_pass_dir}/" pass $@
-  }
-fi
+#if type _pass > /dev/null 2>&1; then
+#  compdef _pass teampass
+#  zstyle ':completion::complete:teampass::' prefix "${__tmp_pass_dir}/"
+#  teampass() {
+#    PASSWORD_STORE_DIR="${__tmp_pass_dir}/" pass $@
+#  }
+#fi
 
 unset __tmp_pass_dir __tmp_pass_name
